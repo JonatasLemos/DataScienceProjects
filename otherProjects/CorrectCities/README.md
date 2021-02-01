7b45564ad29732b05b96c570250532745b289323
Jonatas Oliveira Lemos <satanojlemos@gmail.com>
2021-02-01 01:08:12 -0300

    minor fixes

--- otherProjects/CorrectCities/README.md
+++
@@ -1,4 +0,0 @@
-EN:Algorithm to correct city names using IBGE city names as
-reference.
-PT:Algoritmo para corrigir nomes de cidades usando os nomes do IBGE como
-referência.
--- otherProjects/data_science_course/assignment_one_b.py
+++ otherProjects/data_science_course/assignment_one_b.py
@@ -2,12 +2,8 @@ import re
 with open("logdata.txt", "r",encoding="utf-8") as file:
     logdata = file.read()
 ip = list(re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", logdata))
-# user_name = list(re.findall("\s\w*\d*\s*(?=\[)", logdata))
 user_name = list(re.findall("(?<=-\s).*(?=\s\[)", logdata))
-# time_access = list(re.findall("\d\d/.*/.*:.*:.*:.*\s-\d\d\d\d", logdata))
 time_access = list(re.findall("(?<=\[).*(?=])", logdata))
-# request = list(re.findall("[A-Z]*\s/.\s[A-Z]*/\d\.\d", logdata))
-# request = list(re.findall("\w*\s/.*\s[A-Z]*/\d\.\d", logdata))
 request = list(re.findall("(?<=\").*(?=\")", logdata))
 logs = []
 for item in re.finditer("(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
@@ -17,11 +13,6 @@ for item in re.finditer("(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
     logs.append(item.groupdict())
 print(logs)
 print(len(logs))
-pattern="""
-(?P<host>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})        
-(?P<user_name>(?<=-\s).*(?=\s\[))                        
-(?P<time>(?<=\[).*(?=]))       
-(?P<request>(?<=\").*(?=\"))"""
 print(len(ip))
 # print(ip)
 print(len(time_access))
